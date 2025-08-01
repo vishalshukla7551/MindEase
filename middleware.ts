dcb2fa7b3d71827009aa394c4fef8832bc60ff21
Vishal Shukla <vishalshukla1029@gmail.com>
2025-08-02 05:00:47 +0530

    removing clerk

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isProtectedRoute = createRouteMatcher(['/se(.*)', '/start_page(.*)','/Chats(.*)','/mood-checkin(.*)','/peer_support(.*)'])
+const isProtectedRoute = createRouteMatcher([])
+//['/se(.*)', '/start_page(.*)','/Chats(.*)','/mood-checkin(.*)','/peer_support(.*)']
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect()
